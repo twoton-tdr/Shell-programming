3d6dd48e47bb077c3c7aff00098bf3bef1440c3c
twoton tdr <79171113+twoton-tdr@users.noreply.github.com>
2025-04-03 19:30:55 +0530

    Program to concatenate 2 strings

---
+++ List-3/concatenateString.sh
@@ -0,0 +1,9 @@
+# Program to concatenate 2 string 
+
+echo -n "Enter the first string : "
+read str1
+
+echo -n "Enter the second string : "
+read str2
+
+echo "The concatenated string is $str1 $str2" 
